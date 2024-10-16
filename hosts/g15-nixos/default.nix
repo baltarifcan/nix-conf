6657d7de04df250f68a2ed63e12dd970de407338
Arifcan Balta <baltarifcan@gmail.com>
2024-10-16 17:44:18 +0000

    changes

--- hosts/g15-nixos/default.nix
+++ hosts/g15-nixos/default.nix
@@ -1,24 +1,24 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./modules/system.nix
-      ./hardware-configuration.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./modules/system.nix
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "g15-nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   system.stateVersion = "24.05";
-
 }
-
--- users/baltarifcan/home.nix
+++ users/baltarifcan/home.nix
@@ -1,17 +1,36 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "baltarifcan";
   home.homeDirectory = "/home/baltarifcan";
   home.packages = with pkgs; [
     nix-output-monitor
- ];
- programs.git = {
-  enable = true;
-  userName = "baltarifcan";
-  userEmail = "baltarifcan@gmail.com";  
- };
- home.stateVersion = "24.05";
- programs.home-manager.enable = true;
+    zip
+    unzip
+    p7zip
+    ripgrep
+htop
+libnotify
+xdg-utils
+insomnia
+obsidian
 
+
+  ];
+  programs.git = {
+    userName = "baltarifcan";
+    userEmail = "baltarifcan@gmail.com";
+  };
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+  programs = {
+    firefox = {
+     enable = true;
+     profiles.baltarifcan = {};
+};
+
+
+   };
 }
