b53d8ee29aa5af5e657dc78504aa57f285a7ad22
Arifcan Balta <baltarifcan@gmail.com>
2024-10-16 17:50:05 +0000

    changes

--- hosts/g15-nixos/modules/system.nix
+++ hosts/g15-nixos/modules/system.nix
@@ -85,10 +85,20 @@
   };
   
   hardware.opengl.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings=true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    prime = {
+    sync.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0"; 
+};
   };
   services = {
     dbus.packages = [pkgs.gcr];
